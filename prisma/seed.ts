652c2e3451c89e9b6180590b264c298df178e58b
Maxime Chenot <maxime.c25@gmail.com>
2022-09-11 17:23:26 +0200

    chore: use direct dev migration to seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,22 @@
 import { PrismaClient } from '@prisma/client';
 import * as categories from './data/categories.json';
-import * as products from './data/products.json';
 import * as users from './data/users.json';
 
 const prisma = new PrismaClient();
 
 async function main() {
   await Promise.all([
-    categories.map((c) => prisma.categories.create({ data: c })),
-    products.map((p) => prisma.products.create({ data: p })),
-    users.map((u) => prisma.users.create({ data: u })),
+    ...categories.map((c) =>
+      prisma.categories.create({
+        data: {
+          ...c,
+          products: {
+            create: c.products,
+          },
+        },
+      }),
+    ),
+    ...users.map((u) => prisma.users.create({ data: u })),
   ]);
 }
 
